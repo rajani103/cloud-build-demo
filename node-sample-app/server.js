16c1cea9a0a7451c29a8e904c1462231e4090346
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2024-12-13 02:30:32 +0530

    adding cloudbuild pipeline

---
+++ node-sample-app/server.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const port = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+
+app.get("/", function(req, res) {
+    res.send("App is running.")
+})
+
+app.listen(port, () => {
+    console.log("Server is running on port: " + port)
+})
