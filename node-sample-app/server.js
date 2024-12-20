f0428a1d418120ac06b39e6ad7b943e9a8d29ceb
Rajani Ekunde <43741838+rajani103@users.noreply.github.com>
2024-12-20 21:15:42 +0530

    Update server.js

--- node-sample-app/server.js
+++ node-sample-app/server.js
@@ -8,7 +8,6 @@ app.use(express.json())
 app.get("/", function(req, res) {
     res.send("App is running.")
 }) 
-
 app.listen(port, () => {
     console.log("Server is running on port: " + port)
 })
